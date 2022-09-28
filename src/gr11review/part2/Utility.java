e0139b4bd5b5f512a725450428f7761265f0f037
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-09-28 16:22:51 -0400

    almost done first method

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,6 +1,31 @@
 package gr11review.part2;
 
+import java.io.*;
+
 public class Utility {
+
+    public static int sumNumbers(String str) {
+        int intSum = 0;
+        String strEmpty = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(i)) {
+                if (i < str.length() && Character.isDigit(i++));    
+                strEmpty += i;
+            }
+            else {
+                strEmpty += i;
+                intSum += Integer.parseInt(strEmpty);
+                strEmpty = "";
+            }
+
+        }
+        return intSum;
+    }
+
+    
+
+
 
     
 
