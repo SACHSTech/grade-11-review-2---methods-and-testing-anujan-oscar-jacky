b96dc829682e35edc75579cf645caa2c495f10c3
JackyW19 <jacky.wang23@ycdsbk12.ca>
2022-10-05 10:57:43 -0400

    test commit

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,8 +1,35 @@
 package gr11review.part2;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+ 
 public class Utility {
+ 
+     /**
+     * Given a string, determine if 'xyz' is in the middle
+     *
+     * @param str Given a string, determine if 'xyz' is in the middle
+     * @return true if 'xyz' is in the middle of the string, false otherwise
+     * @author J. Wang
+     */
+    public static boolean xyzMiddle(String str) {
+        int intMiddle = str.length() / 2 - 1;
+ 
+        // check right of middle, if 'xyz' is found, return true
+        if (str.length() >= 3 && (str.substring(intMiddle, intMiddle + 3).equals("xyz"))) {
+            return true;
+        }
+ 
+        // check left of middle, if 'xyz' is found, return true
+        if (str.length() % 2 == 0 && str.substring(intMiddle - 1, intMiddle + 2).equals("xyz")) {
+            return true;
+        }
+ 
+        // return false if xyz is not found in the middle of the string
+        return false;
+   }
+ 
+   }
 
-    
 
-    
-}
+
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -5,6 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import gr11review.part2.Utility;
 
+import java.io.IOError;
+import java.io.IOException;
+
 public class UtilityTest{
-  
+
+    /**
+     * Testing xyzMiddle 
+     * 
+     * @author J. Wang
+     */
+    @Test
+    void xyzMiddleTest1() throws IOException{
+         assertTrue(Utility.xyzMiddle("sidbxyzshda"));
+    }
+      /**
+     * Testing xyzMiddle difference of one
+     * 
+     * @author J. Wang
+     */
+    @Test
+    public void xyzMiddleTest2() throws IOException{
+        assertTrue(Utility.xyzMiddle("POksxyzksl"));
+    }
+          /**
+     * Testing xyzMiddle capital
+     * 
+     * @author J. Wang
+     */
+    
+    @Test
+    public void xyzMiddleTest3() throws IOException{
+        assertTrue(Utility.xyzMiddle("POSDJPXYZOASDJ9"));
+    
+    }
+
 }
